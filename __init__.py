5db4b8f90f011f9acc9bd0d2d516cb4185f794dd
Janzen Houchen-Wilder <janzenwilder39@gmail.com>
2022-11-06 13:58:10 -0800

    adding the __init__ file and SquidAPI updates

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from SquidAPI import SquidAPI
+import os
+
+def create_app():
+    name = __name__
+    file = __file__
+
+    app = SquidAPI()
+
+    @app.route("/")
+    def hello():
+        return None
+
+    return app
+
+def main():
+    app = create_app()
+    from werkzeug import run_simple
+
+    run_simple("127.0.0.1", 5000, app, use_debugger=True, use_reloader=True)
+
+if __name__ == '__main__':
+    main()
